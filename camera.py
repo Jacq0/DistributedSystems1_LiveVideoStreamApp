3a1baecaefffee44ad5461818a3c7a79c18bb6cc
Jacq0 <59923922+Jacq0@users.noreply.github.com>
2022-12-28 23:54:04 +0000

    Added facial recognition

--- camera.py
+++ camera.py
@@ -1,19 +1,64 @@
+from imutils import paths
 import cv2
+import face_recognition as fr
+import pickle
+import os
+import numpy as np
+
 class Camera():
     def __init__(self):        
-        self.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Prepare the camera...
+        self.cap = cv2.VideoCapture(0)  # Prepare the camera...
+        self.classifier = cv2.CascadeClassifier('classifier\haarcascade_frontalface_default.xml') #haarscascade classifier
         print("Camera warming up ...")
-
+        self.encode_faces()
 
     def get_frame(self):
-       ret, img = self.cap.read()
-       ret2, buffer = cv2.imencode('.jpg', img)
+       ret, frame = self.cap.read()
+       frame = self.detect_faces(frame) #do facial detection on frame
 
-       img = buffer.tobytes()
-       #if ret2:  # frame captures without errors...
-           #pass
+       ret2, buffer = cv2.imencode('.jpg', frame)
+    
+       frame = buffer.tobytes()
+       if ret and ret2:  # frame captures without any errors...
+           pass
         
-       return img
+       return frame
 
+    def detect_faces(self, frame):
+        data_enc = pickle.loads(open(os.getcwd() + '\\encodings.pickle', 'rb').read())
+
+        print(data_enc)
+
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_enc = fr.face_encodings(frame_rgb)
+
+        for enc in data_enc:
+            for face in frame_enc:
+                results = fr.compare_faces(np.array(enc['encoding']), face)
+
+                if results[0] == True:
+                    print("Found a recognised face " +  enc['name'])
+
+        return frame
+
+    def encode_faces(self):
+        encoding_data = []
+
+        image_paths = list(paths.list_images(os.getcwd() + '\\face_images'))
+
+        for (i, image_path) in enumerate(image_paths):
+            image, name = cv2.imread(image_path), image_path.split('\\')[-1]
+            imgrgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #dlib expects RGB while opencv uses BGR 
+            boxes = fr.face_locations(imgrgb, model='hog')
+
+            for enc in fr.face_encodings(imgrgb, boxes):
+
+                data = {'encoding': enc, 'name': name}
+                encoding_data.append(data)
+
+        f = open(os.getcwd() + '\\encodings.pickle', 'wb')
+        f.write(pickle.dumps(encoding_data))
+        f.close()
+        
     def release_camera(self):
         self.cap.release()
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, Response
 from camera import Camera
-from django.http import HttpResponse
+from threading import Thread
 
 app=Flask(__name__)
 
@@ -15,7 +15,7 @@ def gen(camera):
          b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
-def video_feed():
+def video_feed(): 
     return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=='__main__':
